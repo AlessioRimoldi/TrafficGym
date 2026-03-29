2e476487f2f1191146476c9aefed0ccf29bc6d24
Diegovano <36131808+Diegovano@users.noreply.github.com>
2026-03-30 01:02:39 +0200

    feat: add service station scenario with no meter

--- src/trafficgym/experiment_sdk/experiments/example/service_station_no_meter.py
+++ src/trafficgym/experiment_sdk/experiments/example/service_station_no_meter.py
@@ -1,21 +1,24 @@
 from trafficgym.experiment_sdk.experiments.base import Experiment
 
 
-class service_station(Experiment):
+class service_station_no_meter(Experiment):
     async def run_experiment(self) -> None:
         tls_id = "TL0"
 
         await self.run.subscribe("simulation", "getTime", None)
+        await self.run.subscribe(
+            "inductionloop", "getLastIntervalOccupancy", "e1_1"
+        )
 
         await self.run.tls.set_program(tls_id, "off")
 
         await self.run.run_time(300)
 
-        controller = await self.run.tls.meter_controller(tls_id, "e1_1")
+        # controller = await self.run.tls.meter_controller(tls_id, "e1_1")
         await self.run.subscribe(
             "inductionloop", "getLastStepOccupancy", "e1_1"
         )
 
         await self.run.run_time(2400)
 
-        controller.cancel()
+        # controller.cancel()
